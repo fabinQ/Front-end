6c47d161bbc96d68a08c485770d960344303bf3e
fabinQ <maciejfabinski@gmail.com>
2024-04-23 23:37:40 +0200

    Wprowadznie do JS, zmienne, dynamiczny paragraf

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-alert("Cześć");
+// alert("Cześć");
+
+const color = "niebieski";
+let age = 24;
+let name = "Lisa";
+console.log(`Cześć, mam na imię ${name} i mam ${age} lata.`);
+
+// Nazwy zmiennych bez liczbt na początku, słow kluczowych, notacja Camel,
+
+const p = document.createElement("p");
+p.textContent = "Dynamiczny paragraf!";
+document.body.append(p);
